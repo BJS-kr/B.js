8a9ea73d83f6d3e94cc836d7b56f6fa55abbea92
BJS-kr <hahajeng1234@gmail.com>
2022-09-18 23:15:09 +0900

    update interpreter

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -4,20 +4,46 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <iostream>
 
 using std::any;
+using std::map;
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
 
+static map<string, Function*> functionTable;
+
+void interpret(Program* program) {
+  for (auto& node: program->functions) {
+    functionTable[node->name] = node;
+  }
+  // nullptr이 NULL 보다 선호되는 이유: https://stackoverflow.com/questions/20509734/null-vs-nullptr-why-was-it-replaced
+  // 간단히 말하자면 0으로 평가될 위험이 없음. 포인터 타입으로만 평가됨
+  if (functionTable["main"] == nullptr) return;
+  functionTable["main"]->interpret();
+}
 /**
  * @brief Statement Interpreters
  */
-void Function::interpret() {};
+void Function::interpret() {
+  for (auto& node: block)
+    node->interpret();
+};
 void Return::interpret() {};
 void Variable::interpret() {};
 void For::interpret() {};
 void Break::interpret() {};
 void Continue::interpret() {};
 void If::interpret() {};
-void Print::interpret() {};
+void Print::interpret() {
+  for (auto& node:arguments) {
+    auto value = node->interpret();
+    cout << value << " ";
+  }
+  cout << endl;
+};
 void ExpressionStatement::interpret() {};
 
 /**
