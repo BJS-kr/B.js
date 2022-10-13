71d56835a8c098d57feefb497d7562c516d68e50
bjs <bjs@bjsui-MacBookAir.local>
2022-10-14 07:43:13 +0900

    master

--- br.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-eval /usr/bin/clang++ \
--std=c++17 \
--fcolor-diagnostics \
--fansi-escape-codes \
--g ./Main.cpp ./Scanner.cpp ./Parser.cpp ./Interpreter.cpp ./Datatype.cpp ./Token.cpp \
--o ./BJS && ./BJS;
